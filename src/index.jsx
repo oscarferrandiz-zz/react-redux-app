f3fd1327b25422dfa911f1432ae6b1c1f57faefb
Oscar Ferrandiz <oscarferrandiz@keymac370.local>
2017-09-06 17:53:45 +0200

    Added fetch polyfill

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,21 @@
+/* Polyfills */
 import 'babel-polyfill';
+import 'whatwg-fetch';
+
+/* Packages */
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+
+/* Store */
 import store from './redux/store';
+
+/* Routes */
 import getRoutes from './routes';
+
+/* Styles */
 import './styles/index.scss';
 
 const history = syncHistoryWithStore(browserHistory, store);
